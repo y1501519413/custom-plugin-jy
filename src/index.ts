7a7b6303547e301040859619bd75ff2cba5b602c
yangjianyang0496 <yangjianyang0496@hhdd.com>
2022-04-11 15:01:45 +0800

    feat: 修改导出配置文件

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { ErrorCode } from './enums/index'
 console.log('ErrorCode', ErrorCode)
 console.log('ErrorCode.NotFoundError', ErrorCode.NotFoundError)
-
-export default (answer: number | string) => {
+const sum = (answer: number | string) => {
   if (typeof answer === 'string') {
     ErrorCode.NotFoundError || undefined
   } else {
     console.log(`the answer is ${answer * 2}`)
   }
 }
+export default sum
