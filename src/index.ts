ea40bd57364926d9ef6e942b542720407136e238
yangjianyang0496 <yangjianyang0496@hhdd.com>
2022-04-13 17:13:26 +0800

    feat: 修改类型文件

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-console.log('ErrorCode', ErrorCode)
 console.log('ErrorCode.NotFoundError', ErrorCode.NotFoundError)
 const sum = (answer: number | string) => {
   if (typeof answer === 'string') {
@@ -7,15 +6,7 @@ const sum = (answer: number | string) => {
     console.log(`the answer is ${answer * 2}`)
   }
 }
-const user: User1 = {
-  name: '小白222333',
-  age: 20,
-  birthday: new Date(),
-}
-console.log('user', user)
 
-for (const key in DEVICE_LEVELS) {
-  console.log('key', key)
-}
+console.log('ErrorCode', ErrorCode.Success)
 
 export default sum
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,22 +3,10 @@ interface User1 {
   age: number
   birthday: Date
 }
-enum INNER_APP_NAME_ENUM {
-  KADA = 'KaDa', // KaDa故事App
-}
-/**
- * 设备性能分级
- */
-enum DEVICE_LEVELS {
-  UNKNOWN = -1, // 未知状态
-  LOW = 0, // 低端机型
-  MIDDLE = 5, // 中端机型
-  HIGH = 10, // 高端机型
-}
 /**
  * 错误码
  */
-enum ErrorCode {
+const enum ErrorCode {
   Success = 200,
   NotFoundError = 404,
   ServerError = 500,
