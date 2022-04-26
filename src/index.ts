c5db15e52b2d44bd42ba2e81ca695fcc87784e9a
yangjianyang0496 <yangjianyang0496@hhdd.com>
2022-04-26 20:55:11 +0800

    feat: iteration

--- src/index.ts
+++ src/index.ts
@@ -33,8 +33,68 @@ const user1: User1 = {
     hobby: 'baseball',
     email: 'adam@gmail.com',
   },
+  info6: {
+    address: 'baseball',
+  },
+  info7: {
+    hobby: 'baseball',
+    email: 'adam@gmail.com',
+  },
 }
-console.log('lodash', lodash.get(user1, 'info.hobby'))
+console.log('lodash', lodash.get(user1, 'info1.hobby'))
 console.log('user1', user1)
+type A = {
+  name: string
+  age: number
+}
+type B = {
+  hobby: string
+}
+type C = A | B
 
+type Foo = Exclude1<C, A>
+// 相当于
+const str: Foo = {
+  hobby: '',
+}
+type User2 = Omit1<
+  User1,
+  'info1' | 'info2' | 'info3' | 'info4' | 'info5' | 'info6' | 'info7'
+>
+const user2: User2 = {
+  name: 'lilith',
+  age: 18,
+  birthday: new Date(),
+}
+
+console.log('user2', user2)
+console.log('str', str)
+
+enum BookSubject {
+  // 养成好习惯
+  GOOD_HABIT = 'good-habit',
+  // 塑造好品格
+  BUILD_PERSONALITY = 'build-personality',
+  // 提升想象力
+  IMAGINE = 'imagine',
+  // 满足好奇心
+  QUIZ = 'quiz',
+  // 赏中华经典
+  CULTURE = 'culture',
+  // 其他
+  OTHER = 'other',
+}
+type GuideBoardItem = {
+  type: BookSubject // 绘本主题
+  title: string // 标题
+  imageUrl: string // 图片
+}
+export const BOOK_SUBJECT_GUIDE_BOARD: Record<BookSubject, GuideBoardItem> = {
+  [BookSubject.GOOD_HABIT]: {
+    type: BookSubject.GOOD_HABIT,
+    title: '养成好习惯',
+    imageUrl:
+      '//cdn.hhdd.com/frontend/as/i/9deaf8b2-a987-51b4-8f2d-94f84d954ad3.png',
+  },
+}
 export default sum
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -13,6 +13,10 @@ type Readonly1<T> = {
 type Pick1<T, K extends keyof T> = {
   [p in K]: T[K]
 }
+type Exclude1<T, U> = T extends U ? never : T
+type Extract1<T, U> = T extends U ? T : never
+type Omit1<T, K> = Pick1<T, Exclude1<keyof T, K>>
+
 type keys = 'hobby' | 'address' | 'email'
 
 type Info1 = Record1<keys, string>
@@ -20,6 +24,8 @@ type Info2 = Partial1<Info1>
 type Info3 = Required1<Info2>
 type Info4 = Readonly1<Info1>
 type Info5 = Pick1<Info1, 'hobby' | 'email'>
+type Info6 = Record1<Exclude1<keys, 'hobby' | 'email'>, string>
+type Info7 = Record1<Extract1<keys, 'hobby' | 'email'>, string>
 
 interface User1 {
   name: string
@@ -30,6 +36,8 @@ interface User1 {
   info3: Info3
   info4: Info4
   info5: Info5
+  info6: Info6
+  info7: Info7
 }
 /**
  * 错误码
